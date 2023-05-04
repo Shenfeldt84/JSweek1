24061847339ad75c0d9f79de20fed21e39c227ca
Mila <123445558+Shenfeldt84@users.noreply.github.com>
2023-05-04 23:18:06 +0600

    Add files via upload

---
+++ week1hw/index.js
@@ -0,0 +1,49 @@
+// Задача № 1
+let cleanerSalary = 40000;
+let singerTeacherSalary = 22000;
+console.log(cleanerSalary + singerTeacherSalary);
+// Задача № 2
+let cityName = "Владивосток";
+let number = "2000";
+console.log(`${cityName + number}`);
+
+let city = "Владивосток";
+let songName = "2000";
+console.log("Владивосток " + "2000");
+// Задача №4
+let degree = 2;
+console.log(degree ** 5);
+// Задача № 5
+let string =
+  "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(string);
+// Задача № 6
+const greetings = "Привет";
+const name = "Кот";
+console.log("Привет " + " Кот!");
+// Задача № 3
+const totalMinutes = 720;
+const hourAmountMinutes = 60;
+console.log(720 / 60);
+// Задача № 7
+
+let time;
+time = 34;
+console.log(time);
+console.log("Старт поездки. Осталось минут:" + time);
+console.log(time - 15);
+time = 19;
+console.log("Немного сториз с соцсетях. Осталось минут:" + time);
+console.log(time - 10);
+time = 9;
+console.log("Немного не новостей, но событий. Осталось минут:" + time);
+console.log(time - 9);
+time = 0;
+console.log("Вы приехали. Добро пожаловать в Мрскву!");
+// Задача №8
+let fahrenheit = 451;
+console.log((fahrenheit - 32) / 1.8);
+let celsius = 232.8;
+console.log(
+  `${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию. `
+);
